20e081ff2d4a0750c18a0d8e29e0a4d978c7b9a8
pengshiquan <1162210866@qq.com>
2018-11-11 19:51:00 +0800

    初始化提交，创建另一个简单的SpringBoot项目
    创建了一个简单的SpringBoot项目

---
+++ src/main/java/com/psq/SpringBootTrain/SpringBootTrainApplication.java
@@ -0,0 +1,12 @@
+package com.psq.SpringBootTrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainApplication.class, args);
+	}
+}
