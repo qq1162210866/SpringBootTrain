921a5181b0f5587500253a3a2399a76fc0d28fc4
pengshiquan <1162210866@qq.com>
2018-11-12 20:01:48 +0800

    提交类文档注释和包名规范
    规范很重要

--- src/main/java/com/psq/SpringBootTrain/controller/HelloWorldController.java
+++ src/main/java/com/psq/SpringBootTrain/controller/HelloWorldController.java
@@ -4,36 +4,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @BelongsProject: SpringBootTrain
- * @BelongsPackage: com.psq.SpringBootTrain.controller
- * @Author: yanyu
- * @CreateTime: 2018-11-11 20:02
- * @Description: 初始化controller,RestController默认以json形式输出
+ * 功能描述:
+ *
+ * @Package: com.psq.SpringBootTrain.controller
+ * @ClassName: HelloWorldController
+ * @auther: pengshiquan
+ * @CreateDate: 2018/11/12 7:56 PM
+ * @UpdateUser: pengshiquan
+ * @UpdateDate: 2018/11/12 7:56 PM
  */
 @RestController
 public class HelloWorldController {
 
-    public static void main(String[] args){
-       /**
-        *
-        * 功能描述:
-        *
-        * @param: [args]
-        * @return: void
-        * @auther: yanyu
-        * @date: 2018/11/11 20:28
-        */
-    }
-    @RequestMapping(value = "/hello")
-    public String HelloWorld(){
+    public static void main(String[] args) {
         /**
          *
-         * 功能描述: helloworld
+         * 功能描述:
          *
-         * @param: null
-         * @return: java.lang.String
+         * @param: [args]
+         * @return: void
          * @auther: yanyu
-         * @date: 2018/11/11 20:37
+         * @date: 2018/11/11 20:28
+         */
+    }
+
+    @RequestMapping(value = "/hello")
+    public String HelloWorld() {
+        /**
+         *
+         * 功能描述:
+         *
+         * @param: []
+         * @return: java.lang.String
+         * @auther: pengshiquan
+         * @date: 2018/11/12 7:16 PM
          */
         return "Hello World";
     }
